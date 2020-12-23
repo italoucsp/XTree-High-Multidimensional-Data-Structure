ef46d0a4d8b36e9b60a61199d886e21b8e98182a
italoucsp <42581127+italoucsp@users.noreply.github.com>
2020-12-23 18:03:26 -0500

    adding topological split and part of split logic, remains overlap minimal split and the rest of the x tree :,v

--- source/XTree.hpp
+++ source/XTree.hpp
@@ -12,6 +12,12 @@
 #define SUPERNODE 2
 #define NO_SPLIT 3
 
+#define SUCCESS 1
+#define FAILURE 0
+
+//COMPARATORS
+#define COMP_ENTRIES_TSPLIT(axis) [=](const std::shared_ptr<XEntry>& A, const std::shared_ptr<XEntry>& B)->bool {return A->MBR.get_min_bound()[axis] < B->MBR.get_min_bound()[axis]; }
+
 template<typename T, size_t N, size_t M>
 class XTree {
 private:
@@ -33,13 +39,13 @@ private:
 
   struct XInternal_Entry :public XEntry {
     std::shared_ptr<XNode> child;
-    XInternal_Entry(std::shared_ptr<XNode> &node);
+    XInternal_Entry(std::shared_ptr<XNode>& node);
     std::shared_ptr<XNode> get_child() override;
   };
 
   struct XLeaf_Entry :public XEntry {
     HyperPoint<T, N> data;
-    XLeaf_Entry(HyperPoint<T, N> &s_data);
+    XLeaf_Entry(HyperPoint<T, N>& s_data);
     HyperPoint<T, N> get_data() override;
   };
 
@@ -48,24 +54,29 @@ private:
     bool is_supernode();
     bool is_leaf();
     void upgrade_to_supernode();
+    std::vector<std::shared_ptr<XEntry>> get_entries_4_test();
   private:
     bool supernode_mode;
     std::vector<std::shared_ptr<XEntry>> entries;
     size_t node_size;
   };
+  HyperRectangle<T, N> calculate_bounding_hyper_rectangle(std::vector<std::shared_ptr<XEntry>> group);
+  double calculate_hyper_margin(std::vector<std::shared_ptr<XEntry>> group);
+  double calculate_hyper_area(std::vector<std::shared_ptr<XEntry>> group);
+  double calculate_overlap_hyper_area(std::vector<std::shared_ptr<XEntry>> group_1, std::vector<std::shared_ptr<XEntry>> group_2);
 
-  void topological_split();
-  size_t choose_split_axis();
-  size_t choose_split_index();
-  void overlap_minimal_split();
-  bool split_node();
-  void insert(XLeaf_Entry &new_sp_obj);
-  void choose_leaf(XLeaf_Entry data_entry, std::stack<std::shared_ptr<XNode>> &parents_path);
+  bool topological_split(std::vector<std::shared_ptr<XEntry>> entries_2_split, std::vector<std::shared_ptr<XEntry>>& set_a, std::vector<std::shared_ptr<XEntry>>& set_b);
+  double choose_split_index(size_t chosen_axis, std::vector<std::shared_ptr<XEntry>> SET_ENTRIES, std::vector<std::shared_ptr<XEntry>>& GA, std::vector<std::shared_ptr<XEntry>>& GB);
+  size_t choose_split_axis(std::vector<std::shared_ptr<XEntry>> SET_ENTRIES);
+  bool overlap_minimal_split(std::vector<std::shared_ptr<XEntry>> entries_2_split, std::vector<std::shared_ptr<XEntry>>& set_a, std::vector<std::shared_ptr<XEntry>>& set_b);
+  bool split_node(std::shared_ptr<XNode>& node_2_split, XLeaf_Entry& data_overflow, std::shared_ptr<XNode>& set_a, std::shared_ptr<XNode>& set_b);
+  void insert(XLeaf_Entry& new_sp_obj);
+  void choose_leaf(XLeaf_Entry data_entry, std::stack<std::shared_ptr<XNode>>& parents_path);
 
   std::shared_ptr<XNode> root;
 public:
   XTree();
-  void assign(std::vector<HyperPoint<T, N>> &raw_data_set);
+  void assign(std::vector<HyperPoint<T, N>>& raw_data_set);
 };
 
 //===============================================XTree Implementation==================================================
@@ -77,7 +88,7 @@ XTree<T, N, M>::XTree() {
 }
 
 template<typename T, size_t N, size_t M>
-void XTree<T, N, M>::assign(std::vector<HyperPoint<T, N>> &raw_data_set) {
+void XTree<T, N, M>::assign(std::vector<HyperPoint<T, N>>& raw_data_set) {
   for (HyperPoint<T, N> hp : raw_data_set) {
     XLeaf_Entry new_entry(hp);
     insert(new_entry);
@@ -85,32 +96,113 @@ void XTree<T, N, M>::assign(std::vector<HyperPoint<T, N>> &raw_data_set) {
 }
 
 template<typename T, size_t N, size_t M>
-void XTree<T, N, M>::topological_split() {
-
+HyperRectangle<T, N> XTree<T, N, M>::calculate_bounding_hyper_rectangle(std::vector<std::shared_ptr<XEntry>> group) {
+  assert(group.size() > 0);
+  HyperRectangle<T, N> BIG_MBR = group[0]->MBR;
+  for each (std::shared_ptr<XEntry> entry in group) {
+    BIG_MBR.expand(entry->MBR);
+  }
+  return BIG_MBR;
 }
 
 template<typename T, size_t N, size_t M>
-size_t XTree<T, N, M>::choose_split_axis() {
-
+double XTree<T, N, M>::calculate_hyper_margin(std::vector<std::shared_ptr<XEntry>> group) {
+  return calculate_bounding_hyper_rectangle(group).get_hyper_margin();
 }
 
 template<typename T, size_t N, size_t M>
-size_t XTree<T, N, M>::choose_split_index() {
-
+double XTree<T, N, M>::calculate_hyper_area(std::vector<std::shared_ptr<XEntry>> group) {
+  return calculate_bounding_hyper_rectangle(group).get_hyper_area();
 }
 
 template<typename T, size_t N, size_t M>
-void XTree<T, N, M>::overlap_minimal_split() {
-
+double XTree<T, N, M>::calculate_overlap_hyper_area(std::vector<std::shared_ptr<XEntry>> group_1, std::vector<std::shared_ptr<XEntry>> group_2) {
+  HyperRectangle<T, N> BIG_MBR_G1 = calculate_bounding_hyper_rectangle(group_1);
+  HyperRectangle<T, N> BIG_MBR_G2 = calculate_bounding_hyper_rectangle(group_2);
+  if (BIG_MBR_G1.get_max_bound() > BIG_MBR_G2.get_min_bound()) {
+    HyperRectangle<T, N> OVERLAP_HYPER_RECT(BIG_MBR_G2.get_min_bound(), BIG_MBR_G1.get_max_bound());
+    return OVERLAP_HYPER_RECT.get_hyper_area();
+  }
+  else if (BIG_MBR_G1.get_min_bound() < BIG_MBR_G2.get_max_bound()) {
+    HyperRectangle<T, N> OVERLAP_HYPER_RECT(BIG_MBR_G1.get_min_bound(), BIG_MBR_G2.get_max_bound());
+    return OVERLAP_HYPER_RECT.get_hyper_area();
+  }
+  else {
+    return 0.0;
+  }
 }
 
 template<typename T, size_t N, size_t M>
-bool XTree<T, N, M>::split_node() {
-
+bool XTree<T, N, M>::topological_split(std::vector<std::shared_ptr<XEntry>> entries_2_split, std::vector<std::shared_ptr<XEntry>>& set_a, std::vector<std::shared_ptr<XEntry>>& set_b) {
+  double overlap = choose_split_index(choose_split_axis(entries_2_split), entries_2_split, set_a, set_b);
+  return (overlap < std::min(calculate_hyper_area(set_a), calculate_hyper_area(set_b)) * MAX_OVERLAP);//if true-> success, else-> failure
 }
 
 template<typename T, size_t N, size_t M>
-void XTree<T, N, M>::insert(XLeaf_Entry &new_sp_obj) {
+size_t XTree<T, N, M>::choose_split_axis(std::vector<std::shared_ptr<XEntry>> SET_ENTRIES) {
+  double min_margin_value = std::numeric_limits<double>::max();
+  size_t best_axis(0);
+  for(size_t axis(0); axis < N; ++axis) {
+    std::sort(SET_ENTRIES.begin(), SET_ENTRIES.end(), COMP_ENTRIES_TSPLIT(axis));
+    size_t n_distributions = M - 2 * MINFANOUT + 2;
+    double current_axis_margin_sum = 0.0;
+    for (size_t k(1); k <= n_distributions; ++k) {
+      std::vector<std::shared_ptr<XEntry>> first_group(SET_ENTRIES.begin(), SET_ENTRIES.begin() + (MINFANOUT - 1 + k));
+      std::vector<std::shared_ptr<XEntry>> second_group(SET_ENTRIES.begin() + (MINFANOUT - 1 + k), SET_ENTRIES.end());
+      current_axis_margin_sum += (calculate_hyper_margin(first_group) + calculate_hyper_margin(second_group));
+    }
+    if (current_axis_margin_sum < min_margin_value) {
+      min_margin_value = current_axis_margin_sum;
+      best_axis = axis;
+    }
+  }
+  return best_axis;
+}
+
+template<typename T, size_t N, size_t M>
+double XTree<T, N, M>::choose_split_index(size_t chosen_axis, std::vector<std::shared_ptr<XEntry>> SET_ENTRIES, std::vector<std::shared_ptr<XEntry>>& GA, std::vector<std::shared_ptr<XEntry>>& GB) {
+  std::sort(SET_ENTRIES.begin(), SET_ENTRIES.end(), COMP_ENTRIES_TSPLIT(chosen_axis));
+  double min_overlap_value = std::numeric_limits<double>::max();
+  size_t n_distributions = M - 2 * MINFANOUT + 2;
+  for (size_t k(1); k <= n_distributions; ++k) {
+    std::vector<std::shared_ptr<XEntry>> first_group(SET_ENTRIES.begin(), SET_ENTRIES.begin() + (MINFANOUT - 1 + k));
+    std::vector<std::shared_ptr<XEntry>> second_group(SET_ENTRIES.begin() + (MINFANOUT - 1 + k), SET_ENTRIES.end());
+    double current_overlap_value = calculate_overlap_hyper_area(first_group, second_group);
+    if (current_overlap_value < min_overlap_value) {
+      min_overlap_value = current_overlap_value;
+      GA.assign(first_group.begin(), first_group.end());
+      GB.assign(second_group.begin(), second_group.end());
+    }
+    else if (current_overlap_value == min_overlap_value) {
+      if ((calculate_hyper_area(first_group) + calculate_hyper_area(second_group)) < (calculate_hyper_area(GA) + calculate_hyper_area(GB))) {
+        GA.assign(first_group.begin(), first_group.end());
+        GB.assign(second_group.begin(), second_group.end());
+      }
+    }
+  }
+  return min_overlap_value;
+}
+
+template<typename T, size_t N, size_t M>
+bool XTree<T, N, M>::overlap_minimal_split(std::vector<std::shared_ptr<XEntry>> entries_2_split, std::vector<std::shared_ptr<XEntry>>& set_a, std::vector<std::shared_ptr<XEntry>>& set_b) {
+  return true;
+}
+
+template<typename T, size_t N, size_t M>
+bool XTree<T, N, M>::split_node(std::shared_ptr<XNode>& node_2_split, XLeaf_Entry& data_overflow, std::shared_ptr<XNode>& set_a, std::shared_ptr<XNode>& set_b) {
+  std::vector<std::shared_ptr<XEntry>> SET_ENTRIES_2_SPLIT(node_2_split->get_entries_4_test());
+  SET_ENTRIES_2_SPLIT.push_back(std::make_shared<XLeaf_Entry>(data_overflow.data));
+  if (topological_split(SET_ENTRIES_2_SPLIT, set_a, set_b) == FAILURE) {
+    set_a.clear(); set_b.clear();
+    if (overlap_minimal_split(SET_ENTRIES_2_SPLIT, set_a, set_b) == FAILURE) {
+      return FAILURE;
+    }
+  }
+  return SUCCESS;
+}
+
+template<typename T, size_t N, size_t M>
+void XTree<T, N, M>::insert(XLeaf_Entry& new_sp_obj) {
   std::stack<std::shared_ptr<XNode>> parents; choose_leaf(new_sp_obj, parents);
   while (!parents.empty()) {
     std::shared_ptr<XNode>& current = parents.top();
@@ -128,7 +220,7 @@ void XTree<T, N, M>::insert(XLeaf_Entry &new_sp_obj) {
 }
 
 template<typename T, size_t N, size_t M>
-void XTree<T, N, M>::choose_leaf(XLeaf_Entry data_entry, std::stack<std::shared_ptr<XNode>> &parents_path) {
+void XTree<T, N, M>::choose_leaf(XLeaf_Entry data_entry, std::stack<std::shared_ptr<XNode>>& parents_path) {
   assert(root != nullptr);
   std::shared_ptr<XNode> candidate_node = root;
   while (!candidate_node->is_leaf()) {
@@ -167,6 +259,11 @@ void XTree<T, N, M>::XNode::upgrade_to_supernode() {
   supernode_mode = true;
 }
 
+template<typename T, size_t N, size_t M>
+std::vector<std::shared_ptr<typename XTree<T, N, M>::XEntry>> XTree<T, N, M>::XNode::get_entries_4_test() {
+  return entries;
+}
+
 //===============================================XEntry Implementation=================================================
 
 template<typename T, size_t N, size_t M>
@@ -175,7 +272,7 @@ XTree<T, N, M>::XEntry::XEntry() {
 }
 
 template<typename T, size_t N, size_t M>
-XTree<T, N, M>::XInternal_Entry::XInternal_Entry(std::shared_ptr<XNode> &node) {
+XTree<T, N, M>::XInternal_Entry::XInternal_Entry(std::shared_ptr<XNode>& node) {
   child = node;
 }
 
@@ -185,7 +282,7 @@ std::shared_ptr<typename XTree<T, N, M>::XNode> XTree<T, N, M>::XInternal_Entry:
 }
 
 template<typename T, size_t N, size_t M>
-XTree<T, N, M>::XLeaf_Entry::XLeaf_Entry(HyperPoint<T, N> &s_data) {
+XTree<T, N, M>::XLeaf_Entry::XLeaf_Entry(HyperPoint<T, N>& s_data) {
   data = s_data;
 }
 
--- source/xtree_utils.hpp
+++ source/xtree_utils.hpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <assert.h>
 #include <iostream>
+#include <limits>
 #include <memory>
 #include <stack>
 #include <string>
@@ -15,6 +16,9 @@ struct HyperPoint {
   HyperPoint& operator=(const HyperPoint<T, N>& other);
   T& operator[](size_t index);
   T operator[](size_t index) const;
+  bool operator==(const HyperPoint<T, N>& other);
+  bool operator<(const HyperPoint<T, N>& other);
+  bool operator>(const HyperPoint<T, N>& other);
 private:
   std::array<T, N> axises;
 };
@@ -31,8 +35,8 @@ HyperPoint<T, N>::HyperPoint(std::array<T, N> m_val) {
 
 template<typename T, size_t N>
 HyperPoint<T, N>& HyperPoint<T, N>::operator=(const HyperPoint<T, N>& other) {
-  for (size_t i(0); i < N; ++i)
-    axises[i] == other.axises[i];
+  for (size_t axis(0); axis < N; ++axis)
+    axises[axis] == other.axises[axis];
   return *this;
 }
 
@@ -49,21 +53,50 @@ T HyperPoint<T, N>::operator[](size_t index) const {
 }
 
 template<typename T, size_t N>
+bool HyperPoint<T, N>::operator==(const HyperPoint<T, N>& other) {
+  bool ok = true;
+  for (size_t axis(0); axis < N; ++axis)
+    ok = ok && (axises[axis] == other.axises[axis]);
+  return ok;
+}
+
+template<typename T, size_t N>
+bool HyperPoint<T, N>::operator<(const HyperPoint<T, N>& other) {
+  bool ok = true;
+  for (size_t axis(0); axis < N; ++axis)
+    ok = ok && (axises[axis] < other.axises[axis]);
+  return ok;
+}
+
+template<typename T, size_t N>
+bool HyperPoint<T, N>::operator>(const HyperPoint<T, N>& other) {
+  bool ok = true;
+  for (size_t axis(0); axis < N; ++axis)
+    ok = ok && (axises[axis] > other.axises[axis]);
+  return ok;
+}
+
+template<typename T, size_t N>
 struct HyperRectangle {
   HyperRectangle() {}
   HyperRectangle(HyperPoint<T, N>& A, HyperPoint<T, N>& B);
   HyperRectangle<T, N>& operator=(const HyperRectangle<T, N>& other);
   bool overlaps(const HyperRectangle<T, N>& other);
   bool contains(const HyperPoint<T, N>& point);
+  double get_hyper_area();
+  double get_hyper_margin();
+  HyperPoint<T, N> get_min_bound();
+  HyperPoint<T, N> get_max_bound();
+  void expand(const HyperRectangle<T, N>& other);
 private:
   HyperPoint<T, N> bottom_left, top_right;
 };
 
 template<typename T, size_t N>
 HyperRectangle<T, N>::HyperRectangle(HyperPoint<T, N>& A, HyperPoint<T, N>& B) {
-  for (size_t i(0); i < N; ++i) {
-    bottom_left[i] = std::min(A[i], B[i]);
-    top_right[i] = std::max(A[i], B[i]);
+  for (size_t axis(0); axis < N; ++axis) {
+    bottom_left[axis] = std::min(A[axis], B[axis]);
+    top_right[axis] = std::max(A[axis], B[axis]);
   }
 }
 
@@ -76,9 +109,9 @@ HyperRectangle<T, N>& HyperRectangle<T, N>::operator=(const HyperRectangle<T, N>
 
 template<typename T, size_t N>
 bool HyperRectangle<T, N>::overlaps(const HyperRectangle<T, N>& other) {
-  for (size_t i(0); i < N; ++i) {
-    if (!(bottom_left[i] <= other.top_right[i] &&
-      top_right[i] >= other.bottom_left[i])) {
+  for (size_t axis(0); axis < N; ++axis) {
+    if (!(bottom_left[axis] <= other.top_right[axis] &&
+      top_right[axis] >= other.bottom_left[axis])) {
       return false;
     }
   }
@@ -87,9 +120,45 @@ bool HyperRectangle<T, N>::overlaps(const HyperRectangle<T, N>& other) {
 
 template<typename T, size_t N>
 bool HyperRectangle<T, N>::contains(const HyperPoint<T, N>& point) {
-  for (size_t i(0); i < N; ++i) {
-    if (!(bottom_left[i] <= point[i] && point[i] <= top_right[i]))
+  for (size_t axis(0); axis < N; ++axis) {
+    if (!(bottom_left[axis] <= point[axis] && point[axis] <= top_right[axis]))
       return false;
   }
   return true;
+}
+
+template<typename T, size_t N>
+double HyperRectangle<T, N>::get_hyper_area() {
+  double hyper_area = double(!(bottom_left == top_right));
+  for (size_t axis(0); axis < N; ++axis) {
+    hyper_area *= (top_right[axis] - bottom_left[axis]);
+  }
+  return hyper_area;
+}
+
+template<typename T, size_t N>
+double HyperRectangle<T, N>::get_hyper_margin() {
+  double hyper_margin = 0.0, dim_margin_m = (1 << (N - 1));
+  for (size_t axis(0); axis < N; ++axis) {
+    hyper_margin += (top_right[axis] - bottom_left[axis]);
+  }
+  return hyper_margin * dim_margin_m;
+}
+
+template<typename T, size_t N>
+HyperPoint<T, N> HyperRectangle<T, N>::get_min_bound() {
+  return bottom_left;
+}
+
+template<typename T, size_t N>
+HyperPoint<T, N> HyperRectangle<T, N>::get_max_bound() {
+  return top_right;
+}
+
+template<typename T, size_t N>
+void HyperRectangle<T, N>::expand(const HyperRectangle<T, N>& other) {
+  for (size_t axis(0); axis < N; ++axis) {
+    bottom_left[axis] = std::min(bottom_left[axis], other.bottom_left[axis]);
+    top_right[axis] = std::max(top_right[axis], other.top_right[axis]);
+  }
 }
